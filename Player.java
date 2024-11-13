a993b350272b8fcfc375475295d2cb91efa015d3
Rodrigo Rodrigues <eusourodrigo895@gmail.com>
2024-11-13 09:29:20 -0300

    up

---
+++ Player.java
@@ -0,0 +1,42 @@
+public class Player{
+    private String name;
+    private int x, y;
+
+    //Constructor
+    public Player(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Get'n Set Methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    //Methods
+
+
+    
+    
+}
